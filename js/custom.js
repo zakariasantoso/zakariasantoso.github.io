76d2a30ad9723e9e9d7fc3f03b757ef635296c00
zakariasantoso <santosozakaria29@gmail.com>
2021-04-07 10:08:47 +0700

    http(s)://

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,34 @@
-var customScripts={onePageNav:function(){$("#main-nav ul li:first-child").hasClass("active")&&$("#main-nav").css("background","none"),$("#mainNav").onePageNav({currentClass:"active",changeHash:!1,scrollSpeed:950,scrollThreshold:.2,filter:"",easing:"swing",begin:function(){},end:function(){$("#main-nav ul li:first-child").hasClass("active")?$(".header").removeClass("addBg"):$(".header").addClass("addBg")},scrollChange:function(a){$("#main-nav ul li:first-child").hasClass("active")?$(".header").removeClass("addBg"):$(".header").addClass("addBg")}})},init:function(){customScripts.onePageNav()}};$("document").ready(function(){customScripts.init(),$(window).load(function(){$("#filter .current").trigger("click")})});
+var customScripts = {
+  onePageNav: function () {
+    $("#main-nav ul li:first-child").hasClass("active") &&
+      $("#main-nav").css("background", "none"),
+      $("#mainNav").onePageNav({
+        currentClass: "active",
+        changeHash: !1,
+        scrollSpeed: 950,
+        scrollThreshold: 0.2,
+        filter: "",
+        easing: "swing",
+        begin: function () {},
+        end: function () {
+          $("#main-nav ul li:first-child").hasClass("active")
+            ? $(".header").removeClass("addBg")
+            : $(".header").addClass("addBg");
+        },
+        scrollChange: function (a) {
+          $("#main-nav ul li:first-child").hasClass("active")
+            ? $(".header").removeClass("addBg")
+            : $(".header").addClass("addBg");
+        },
+      });
+  },
+  init: function () {
+    customScripts.onePageNav();
+  },
+};
+$("document").ready(function () {
+  customScripts.init(),
+    $(window).load(function () {
+      $("#filter .current").trigger("click");
+    });
+});
